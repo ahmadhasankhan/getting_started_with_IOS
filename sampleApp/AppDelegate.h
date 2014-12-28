54f16a3ea9565673488f5ad3c5c295c1b44a2486
ahmadhasankhan <ahmad.hassan@olacabs.com>
2014-12-28 23:09:04 +0530

    Initial Commit

---
+++ sampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleApp
+//
+//  Created by Ahmad Hassan on 28/12/14.
+//  Copyright (c) 2014 Ahmad Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleApp
+//
+//  Created by Ahmad Hassan on 28/12/14.
+//  Copyright (c) 2014 Ahmad Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
